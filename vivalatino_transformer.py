5fce5cae6a8e1cf93c53ff2fd1d0fb0409008046
alaiy <alai95@icloud.com>
2022-03-30 09:17:00 +0100

    edit

--- vivalatino_transformer.py
+++ vivalatino_transformer.py
@@ -3,7 +3,7 @@ import spotipy
 
 spotipy_object = spotipy.Spotify(client_credentials_manager=spotipy.oauth2.SpotifyClientCredentials())
 
-
+# Read through artists from playlist
 def get_artists(filepath=
                 '/Users/alaiy/Documents/Projects/etl_Stuff/aws_spotify_etl/vivalatino_artists.csv'):
     with open(filepath, 'r') as file:
